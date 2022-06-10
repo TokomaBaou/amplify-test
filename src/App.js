89a0a673304612df6d59323ea4de9acd5ba3822c
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2022-06-11 00:20:01 +0900

    ログイン処理

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import "./styles.css";
+import { useEffect, useState } from "react";
+import Amplify from "aws-amplify";
+import { AmplifySignOut, AmplifyAuthenticator } from "@aws-amplify/ui-react";
+import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
+// import awsconfig from './aws-exports'
 
-export default function App() {
-  return (
+export const App = () => {
+  /** ログインされているかどうか判断するためAuth*/
+  const [authState, setAuthState] = useState();
+  /**ユーザー情報を保持する */
+  const [user, setUser] = useState();
+
+  /**loginの状態に変更があった時にステートにセットする */
+  useEffect(() => {
+    /**アンマウント時に処理をクリアしたい */
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
+  }, []);
+  /**authStateとAuthStateコンポーネントのSignedInの値が同じであり、
+   * userが存在する時ログイン後の画面を表示する */
+  return authState === AuthState.SignedIn && user ? (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <AmplifySignOut />
+      <h2>Component</h2>
     </div>
+  ) : (
+    <AmplifyAuthenticator />
   );
-}
+};
